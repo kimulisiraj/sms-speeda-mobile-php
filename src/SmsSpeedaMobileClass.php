ccacf3441ce77f4f5b6b30d93af20e31398f9d97
Siraj Kimuli <pworld.designer@gmail.com>
2022-02-10 02:13:41 +0300

    prefer only stable

--- src/SmsSpeedaMobileClass.php
+++ src/SmsSpeedaMobileClass.php
@@ -4,4 +4,16 @@ namespace Kimulisiraj\SmsSpeedaMobile;
 
 class SmsSpeedaMobileClass
 {
+    public const API_URL = 'http://apidocs.speedamobile.com/api/SendSMS';
+
+    public static function conf(string $apiKey, string $apiSecret): self
+    {
+        return new static($apiKey, $apiSecret);
+    }
+
+    public function __construct(
+        protected string $apiKey,
+        protected string $apiSecret,
+    ) {
+    }
 }
