7e7a52badcac10b058700e42f2a97bad2026a012
macdanson <macdanson2@gmail.com>
2024-10-15 20:19:36 +0300

    Added `messageStatus`, `messageId` methods required to query message status

--- src/SmsSpeedaMobile.php
+++ src/SmsSpeedaMobile.php
@@ -3,13 +3,18 @@
 namespace Kimulisiraj\SmsSpeedaMobile;
 
 use GuzzleHttp\RequestOptions;
+use JsonException;
 use Kimulisiraj\SmsSpeedaMobile\Api\Client;
+use Kimulisiraj\SmsSpeedaMobile\Api\MessageStatusResponse;
 use Kimulisiraj\SmsSpeedaMobile\Api\SendResponse;
+use Kimulisiraj\SmsSpeedaMobile\Exceptions\SendException;
+use Kimulisiraj\SmsSpeedaMobile\Exceptions\SmsBroadcastException;
 
 class SmsSpeedaMobile
 {
     protected string $to = '';
     protected string $message = '';
+    protected string $messageId = '';
     protected Client $client;
 
     public static function config(string $apiKey, string $apiPassword): Client
@@ -39,7 +44,7 @@ class SmsSpeedaMobile
     }
 
     /**
-     * @throws Exceptions\SendException
+     * @throws Exceptions\SendException|JsonException
      */
     public function send(string $to = '', string $message = ''): SendResponse
     {
@@ -49,6 +54,16 @@ class SmsSpeedaMobile
         return $this->client->send($to, $this->message);
     }
 
+    /**
+     * @throws SendException|JsonException
+     */
+    public function messageStatus(string $messageId = ''): MessageStatusResponse
+    {
+        $this->messageId = $messageId;
+
+        return $this->client->messageStatus($messageId);
+    }
+
     public function to(string $to): self
     {
         $this->to = $to;
@@ -56,6 +71,10 @@ class SmsSpeedaMobile
         return $this;
     }
 
+    /**
+     * @throws SmsBroadcastException
+     * @throws JsonException
+     */
     public function getBalance(): array
     {
         return $this->client->getBalance();
@@ -64,6 +83,13 @@ class SmsSpeedaMobile
     public function message(string $message): self
     {
         $this->message = $message;
+
+        return $this;
+    }
+
+    public function messageId(string $messageId): self
+    {
+        $this->messageId = $messageId;
 
         return $this;
     }
