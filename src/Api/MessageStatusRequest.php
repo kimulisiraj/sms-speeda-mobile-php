938a806b3e1070692540e0f2e0e072788a19eba3
macdanson <macdanson2@gmail.com>
2024-10-15 20:22:13 +0300

    This `MessageStatusRequest` class handles querying the message status

---
+++ src/Api/MessageStatusRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Kimulisiraj\SmsSpeedaMobile\Api;
+
+use Kimulisiraj\SmsSpeedaMobile\Exceptions\InvalidMessageException;
+
+class MessageStatusRequest
+{
+    public function __construct(
+        private string  $messageId,
+    ) {
+    }
+
+    /**
+     * @throws InvalidMessageException
+     */
+    public function validate(): void
+    {
+        if (empty($this->messageId)) {
+            throw new InvalidMessageException('No `messageId` provided');
+        }
+    }
+
+    /**
+     * @return array{message_id: string}
+     */
+    public function toRequest(): array
+    {
+        return [
+            "message_id" => $this->messageId
+        ];
+    }
+}
